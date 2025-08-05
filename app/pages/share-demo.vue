bce3455eb0819aa965b0cf2a5cfcde4a292a847f
LiuLei <liuleiytu@gmail.com>
2025-08-05 22:21:21 +0800

    init

---
+++ app/pages/share-demo.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import {useRoute} from "#imports";
+
+const route = useRoute()
+
+const url = route.query.url as string
+</script>
+
+<template>
+<div>
+  <template v-if="url">
+    <Head>
+      <Meta property="og:title" content=""/>
+      <Meta property="og:description" content=""/>
+      <Meta property="twitter:title" content=""/>
+      <Meta property="twitter:description" content=""/>
+      <Meta property="og:site_name" content=""/>
+      <Meta property="og:type" content="website"/>
+      <Meta property="twitter:card" content="summary_large_image"/>
+      <Meta property="twitter:site" content="@Demo"/>
+      <Meta property="twitter:creator" content="@Demo"/>
+      <Meta property="twitter:image" :content="url"/>
+      <Meta property="og:image" :content="url"/>
+    </Head>
+  </template>
+  share demo</div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ app/pages/share.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const imageUrl = ref('')
+const share = (type:'x' | 'facebook') => {
+  const shareUrl = window.location.origin + `/share-demo?url=${imageUrl.value}`
+  switch (type){
+    case "facebook":
+      window.open(`http://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(shareUrl)}`,'_blank')
+      return
+    case "x":
+      window.open(`https://twitter.com/intent/tweet?url=${shareUrl}`,'_blank')
+      return;
+  }
+}
+</script>
+
+<template>
+<div>
+  <div>
+    <input v-model="imageUrl"/>
+  </div>
+  <button @click="share('x')">share x</button>
+  <button @click="share('facebook')">share Facebook</button>
+</div>
+</template>
+
+<style scoped>
+
+</style>
