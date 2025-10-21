f5a09cb141f25918fef1a60ae6fe156fd43d0a93
LiuLei <liuleiytu@gmail.com>
2025-10-21 08:19:54 +0800

    test

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,43 @@
 <template>
-  <NuxtPage />
+  <div>
+    <Head>
+      <Meta property="og:title" content="加密貨幣衍生品交易所 | 購買，出售，交易加密貨幣 | Bitunix"/>
+      <Meta name="twitter:title" content="加密貨幣衍生品交易所 | 購買，出售，交易加密貨幣 | Bitunix"/>
+      <Meta property="og:description"
+            content="Bitunix - 領先的加密貨幣衍生品交易所，輕鬆購買、出售、交易BTC等加密貨幣，並隨時隨地交易衍生品。"/>
+      <Meta name="twitter:description"
+            content="Bitunix - 領先的加密貨幣衍生品交易所，輕鬆購買、出售、交易BTC等加密貨幣，並隨時隨地交易衍生品。"/>
+      <Meta name="keywords" content="Bitunix,Bitunix 官網,Crypto Exchange,Cryptocurrency Exchange,Bitcoin Exchange"/>
+      <Meta name="description"
+            content="Bitunix - 領先的加密貨幣衍生品交易所，輕鬆購買、出售、交易BTC等加密貨幣，並隨時隨地交易衍生品。"/>
+      <Meta property="og:site_name" content="Bitunix"/>
+      <Meta property="og:type" content="website"/>
+      <Meta name="twitter:card" content="summary_large_image"/>
+      <Meta name="twitter:site" content="@Bitunix"/>
+      <Meta name="twitter:creator" content="@Bitunix"/>
+      <Meta name="twitter:image" content="https://static.bitunix.com/banner/share-banner-1200.png"/>
+      <Meta property="og:image" content="https://static.bitunix.com/banner/share-banner-1200.png"/>
+      <Meta hid="i18n-og-url" property="og:url" content="https://www.bitunix.com/zh-tw"/>
+      <Meta hid="i18n-og" property="og:locale" content="zh_tw"/>
+      <Meta hid="i18n-og-alt-de-de" property="og:locale:alternate" content="de_de"/>
+      <Meta hid="i18n-og-alt-en-us" property="og:locale:alternate" content="en_us"/>
+      <Meta hid="i18n-og-alt-es-es" property="og:locale:alternate" content="es_es"/>
+      <Meta hid="i18n-og-alt-fa-ir" property="og:locale:alternate" content="fa_ir"/>
+      <Meta hid="i18n-og-alt-fr-fr" property="og:locale:alternate" content="fr_fr"/>
+      <Meta hid="i18n-og-alt-hi-in" property="og:locale:alternate" content="hi_in"/>
+      <Meta hid="i18n-og-alt-id-id" property="og:locale:alternate" content="id_id"/>
+      <Meta hid="i18n-og-alt-it-it" property="og:locale:alternate" content="it_it"/>
+      <Meta hid="i18n-og-alt-ja-jp" property="og:locale:alternate" content="ja_jp"/>
+      <Meta hid="i18n-og-alt-pl-pl" property="og:locale:alternate" content="pl_pl"/>
+      <Meta hid="i18n-og-alt-pt-br" property="og:locale:alternate" content="pt_br"/>
+      <Meta hid="i18n-og-alt-pt-pt" property="og:locale:alternate" content="pt_pt"/>
+      <Meta hid="i18n-og-alt-ru-ru" property="og:locale:alternate" content="ru_ru"/>
+      <Meta hid="i18n-og-alt-uz-uz" property="og:locale:alternate" content="uz_uz"/>
+      <Meta hid="i18n-og-alt-vi-vn" property="og:locale:alternate" content="vi_vn"/>
+      <Meta name="robots" content="follow, index, max-snippet:-1, max-video-preview:-1, max-image-preview:large"/>
+    </Head>
+    <NuxtPage />
+  </div>
 </template>
+<script setup lang="ts">
+</script>
--- app/pages/share-demo.vue
+++ app/pages/share-demo.vue
@@ -7,36 +7,15 @@ const url = route.query.url as string
 </script>
 
 <template>
-<div>
-  <template v-if="url">
-    <Head>
-<!--      <Meta property="og:title" content=""/>
-      <Meta property="og:description" content=""/>
-      <Meta property="twitter:title" content=""/>
-      <Meta property="twitter:description" content=""/>
-      <Meta property="og:site_name" content=""/>
-      <Meta property="og:type" content="website"/>
-      <Meta property="twitter:card" content="summary_large_image"/>
-      <Meta property="twitter:site" content="@Demo"/>
-      <Meta property="twitter:creator" content="@Demo"/>
-      <Meta property="twitter:image" :content="url"/>
-      <Meta property="og:image" :content="url"/>-->
-
-      <Meta property="og:title" content="加密衍生品交易所 | 買賣和交易加密貨幣 | Bitunix"/>
-      <Meta property="og:description" content="Bitunix - 領先的加密衍生品交易所，讓您的交易更輕鬆。買入、賣出、交易 BTC 和另類貨幣，並輕鬆進入期貨市場。"/>
-      <Meta name="twitter:description" content="Bitunix - 領先的加密衍生品交易所，讓您的交易更輕鬆。買入、賣出、交易 BTC 和其他代幣，並輕鬆進入期貨市場"/>
-      <Meta name="keywords" content="Bitunix,Bitunix 官網,Crypto Exchange,Cryptocurrency Exchange,Bitcoin Exchange"/>
-      <Meta name="description" content="Bitunix - 領先的加密貨幣衍生品交易所，輕鬆購買、出售、交易BTC等加密貨幣，並隨時隨地交易衍生品。"/>
-      <Meta property="og:site_name" content="Bitunix"/>
-      <Meta property="og:type" content="website"/>
-      <Meta name="twitter:card" content="summary_large_image"/>
-      <Meta name="twitter:site" content="@Bitunix"/>
-      <Meta name="twitter:creator" content="@Bitunix"/>
-      <Meta name="twitter:image" :content="url"/>
-      <Meta property="og:image" :content="url"/>
-    </Head>
-  </template>
-  share demo</div>
+  <div>
+    <template v-if="url">
+      <Head>
+        <Meta name="twitter:image" :content="url"/>
+        <Meta property="og:image" :content="url"/>
+      </Head>
+    </template>
+    share demo
+  </div>
 </template>
 
 <style scoped>
