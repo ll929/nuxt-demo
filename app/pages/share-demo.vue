85ad3f000090ed564f70ed948476270e9eccd524
LiuLei <liuleiytu@gmail.com>
2025-10-21 09:13:16 +0800

    test

--- app/pages/share-demo.vue
+++ app/pages/share-demo.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import {useRoute} from "#imports";
+import {useRoute,useRequestURL} from "#imports";
 
 const route = useRoute()
 
 const url = route.query.url as string
+const fullUrl = useRequestURL().href;
 </script>
 
 <template>
@@ -12,6 +13,7 @@ const url = route.query.url as string
       <Head>
         <Meta name="twitter:image" :content="url"/>
         <Meta property="og:image" :content="url"/>
+        <Meta hid="i18n-og-url" property="og:url" :content="fullUrl"/>
       </Head>
     </template>
     share demo
